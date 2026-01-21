614cc33a227ee70f35bd97c68700a15b1a91a4ea
Pierre Dok <pierredok@outlook.fr>
2026-01-21 12:06:42 +0100

    adding randomize background olor

--- components/intro/index.tsx
+++ components/intro/index.tsx
@@ -1,62 +1,67 @@
 "use client";
 
-import { Suspense } from "react";
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 
 export default function Intro() {
-  const [showIntro] = useState(() => {
-    if (typeof window === "undefined") return false;
-    return !sessionStorage.getItem("hasSeenH1Animate");
-  });
+  const [showIntro, setShowIntro] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
+  const [isVisibleBg, setIsVisibleBg] = useState(true);
+  const [isVisibleH1, setIsVisibleH1] = useState(true);
 
-  const [isVisibleBg, setIsVisibleBg] = useState(showIntro);
-  const [isVisibleH1, setIsVisibleH1] = useState(showIntro);
+  const [randomBg, setRandomBg] = useState("#998D77");
 
   useEffect(() => {
-    if (!showIntro) return;
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setIsMounted(true);
+    const hasSeenIntro = sessionStorage.getItem("hasSeenH1Animate");
 
-    sessionStorage.setItem("hasSeenH1Animate", "true");
+    if (!hasSeenIntro) {
+      const colors = ["#998D77", "#9EBEE4"];
+      const chosenColor = colors[Math.floor(Math.random() * colors.length)];
+      setRandomBg(chosenColor);
 
-    const timerBg = setTimeout(() => {
-      setIsVisibleBg(false);
-    }, 1500);
+      setShowIntro(true);
 
-    const timerH1 = setTimeout(() => {
-      setIsVisibleH1(false);
-      sessionStorage.setItem("hasSeenH1Animate", "true");
-    }, 1150);
+      const timerH1 = setTimeout(() => {
+        setIsVisibleH1(false);
+      }, 1150);
 
-    return () => {
-      clearTimeout(timerBg);
-      clearTimeout(timerH1);
-    };
-  }, [showIntro]);
+      const timerBg = setTimeout(() => {
+        setIsVisibleBg(false);
+        sessionStorage.setItem("hasSeenH1Animate", "true");
+      }, 1500);
 
-  if (!showIntro) return null;
+      return () => {
+        clearTimeout(timerH1);
+        clearTimeout(timerBg);
+      };
+    }
+  }, []);
+
+  if (!isMounted || !showIntro) return null;
 
   return (
-    <>
-      {showIntro && (
-        <Suspense>
-          <div>
-            <motion.div
-              className={`fixed inset-0 z-45 bg-[#998D77] ${isVisibleBg ? "block" : "hidden"}`}
-              initial={{ x: 0 }}
-              animate={{ x: "+100%" }}
-              transition={{ duration: 0.5, delay: 1, ease: "easeOut" }}
-            >
-              <motion.div
-                className={`fixed z-50 mt-20 mb-20 inset-0 flex items-center justify-center ${isVisibleH1 ? "block" : "hidden"}`}
-              >
-                <h1 className="font-neueGrotesk text-[#FAEEBC] dekstop:text-[70px] tablet:text-[45px] text-[35px]">
-                  Valentin Valette
-                </h1>
-              </motion.div>
-            </motion.div>
-          </div>
-        </Suspense>
-      )}
-    </>
+    <div className="fixed inset-0 z-50 pointer-events-none">
+      <motion.div
+        className="fixed inset-0 z-45"
+        style={{ backgroundColor: randomBg }}
+        initial={{ x: 0 }}
+        animate={{ x: "100%" }}
+        transition={{ duration: 0.5, delay: 1, ease: "easeOut" }}
+      >
+        <motion.div
+          className={`fixed inset-0 flex items-center justify-center ${
+            isVisibleH1
+              ? "opacity-100"
+              : "opacity-0 transition-opacity duration-300"
+          }`}
+        >
+          <h1 className="font-neueGrotesk text-[#FAEEBC] desktop:text-[70px] tablet:text-[45px] text-[35px]">
+            Valentin Valette
+          </h1>
+        </motion.div>
+      </motion.div>
+    </div>
   );
 }
