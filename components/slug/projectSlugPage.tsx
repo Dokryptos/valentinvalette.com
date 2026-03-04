f9be26a42044c00ee085f7fe06d531ac21a34179
Dokryptos <pierredok@outlook.fr>
2026-03-04 02:24:17 +0100

    adding slug tablet

--- components/slug/projectSlugPage.tsx
+++ components/slug/projectSlugPage.tsx
@@ -17,9 +17,8 @@ export default function ProjectSlugPage({ projectData }: ProjectSlugPageProps) {
   const selectedImage = images[selectedImageIndex];
 
   // Largeur réelle d'un item (w-48 = 192px + gap-3 = 12px)
-  const ITEM_WIDTH = 204;
+  const ITEM_WIDTH = 48;
 
-  // Fonction pour scroller proprement
   const scrollToThumb = (index: number) => {
     if (scrollContainerRef.current) {
       scrollContainerRef.current.scrollTo({
@@ -35,27 +34,29 @@ export default function ProjectSlugPage({ projectData }: ProjectSlugPageProps) {
   };
 
   return (
-    <Grid className="min-h-screen overflow-hidden font-SuisseIntl w-full">
-      <div className="fixed top-11 md:top-4 left-5 z-30 col-span-full md:col-span-4 ">
-        <h1 className="text-[25px] xl:text-[35px] font-PPeditorialNew">
-          {projectData.title}
-        </h1>
-        <p className="text-[11px] xl:text-[15px]">{projectData.category}</p>
-      </div>
-
-      <div className="flex flex-col col-span-full md:col-span-8 ">
-        <div className="flex items-center pt-20">
-          {selectedImage && (
-            <UIImageSanity
-              asset={selectedImage.asset}
-              alt="Selected"
-              className="object-contain px-4"
-            />
-          )}
+    <>
+      <Grid className="min-h-screen overflow-hidden font-SuisseIntl w-full">
+        <div className="fixed top-11 md:top-4 left-5 z-30 col-span-full md:col-span-4 ">
+          <h1 className="text-[25px] xl:text-[35px] font-PPeditorialNew">
+            {projectData.title}
+          </h1>
+          <p className="text-[11px] xl:text-[15px]">{projectData.category}</p>
         </div>
 
-        
-        <Grid className="flex text-[10px] uppercase px-4">
+        <div className="flex flex-col col-span-full md:col-span-4 md:col-start-7 lg:col-start-7 lg:col-span-4">
+          <div className="flex items-center pt-20 lg:pt-7.5">
+            {selectedImage && (
+              <UIImageSanity
+                asset={selectedImage.asset}
+                alt="Selected"
+                className="object-contain px-4 md:px-0 max-h-[70vh] md:pr-4 lg:pr-0"
+              />
+            )}
+          </div>
+        </div>
+      </Grid>
+      <div className="fixed bottom-0 left-0 w-full px-4 pb-4 pt-2">
+        <Grid className="flex text-[10px] uppercase mb-2">
           <button
             className="col-start-1 text-left"
             onClick={() =>
@@ -66,11 +67,11 @@ export default function ProjectSlugPage({ projectData }: ProjectSlugPageProps) {
           >
             Prev
           </button>
-          <span className="col-start-5 text-left">
+          <span className="col-start-5 md:col-start-6 lg:col-start-7 text-left">
             {selectedImageIndex + 1} / {images.length}
           </span>
           <button
-            className="col-start-8 text-end"
+            className="col-start-8 md:col-start-10 lg:col-start-12 text-end"
             onClick={() =>
               handleCarouselClick((selectedImageIndex + 1) % images.length)
             }
@@ -96,12 +97,12 @@ export default function ProjectSlugPage({ projectData }: ProjectSlugPageProps) {
               <UIImageSanity
                 asset={image.asset}
                 alt="thumb"
-                className="w-auto h-16 object-contain"
+                className="w-auto h-16 md:h-22 object-contain"
               />
             </button>
           ))}
         </div>
       </div>
-    </Grid>
+    </>
   );
 }
