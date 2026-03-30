609e4bcc0fcc5ae56db65b769a399d4a2f6fcd3d
Dokryptos <pierredok@outlook.fr>
2026-03-31 00:33:57 +0200

    adding writing DL PDF

--- app/about/writings/page.tsx
+++ app/about/writings/page.tsx
@@ -5,14 +5,22 @@ export default async function WritingsPage({}) {
   return (
     <div>
       {writings.map((writing) => {
+        const fileName = writing.description
+          ? writing.description.replace(/\s+/g, "_").substring(0, 30)
+          : "document";
+        const downloadUrl = writing.fileUrl
+          ? `${writing.fileUrl}?dl=${fileName}.pdf`
+          : "#";
         return (
-          <div
+          <a
             className="flex gap-4 mb-4 text-[11px] xl:text-[15px] font-SuisseIntl"
             key={writing._id}
+            href={downloadUrl}
+            download
           >
             <span>{writing.year}</span>
             <p>{writing.description}</p>
-          </div>
+          </a>
         );
       })}
     </div>
--- types/about.ts
+++ types/about.ts
@@ -1,6 +1,12 @@
 import type { Slug } from "@sanity/types";
 
 export default interface AboutDescription {
+  fileUrl: {
+    asset: {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      url: any;
+    };
+  };
   _id: string;
   slug: Slug;
   description: string;
