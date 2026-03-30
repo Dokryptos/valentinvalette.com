61426092096a0e364c56d61551e9f5db27e08e1b
Dokryptos <pierredok@outlook.fr>
2026-03-31 00:05:20 +0200

    range grid logic, text animation slug with popup

--- app/about/writings/page.tsx
+++ app/about/writings/page.tsx
@@ -7,16 +7,13 @@ export default async function WritingsPage({}) {
     <div>
       {writings.map((writing) => {
         return (
-          <a
-            download
+          <div
             className="flex gap-4 mb-4 text-[11px] xl:text-[15px] font-SuisseIntl"
             key={writing._id}
-            href={writing.downloadFile?.asset.url || "#"}
-            rel="noopener noreferrer"
           >
             <span>{writing.year}</span>
             <p>{writing.description}</p>
-          </a>
+          </div>
         );
       })}
     </div>
